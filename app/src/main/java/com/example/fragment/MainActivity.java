fbbfb4123324fde52e38f5226a2119c278f2a4de
unknown <saadh9290@gmail.com>
2022-01-16 06:33:31 -0700

    fragment 2

--- app/src/main/java/com/example/fragment/MainActivity.java
+++ app/src/main/java/com/example/fragment/MainActivity.java
@@ -4,13 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
 
-    Button fr;
+    Button fr,button2;
     LinearLayout layout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,9 +19,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
          fr=findViewById(R.id.button);
          layout=findViewById(R.id.lin);
-        Fragment1 fragment1 = new Fragment1();
-        FragmentTransaction transaction =getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.layout, fragment1);
-        transaction.commit();
+        fr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment1 fragment1 = new Fragment1();
+                FragmentTransaction transaction =getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.layout, fragment1);
+                transaction.commit();
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment2 fragment2 = new Fragment2();
+                FragmentTransaction transaction =getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.layout, fragment2);
+                transaction.commit();
+            }
+        });
     }
 }
